4d529ead17f414daeb1958c1842bf5a4c9a10355
JzK <andre-kiryushin@mail.ru>
2024-07-23 15:59:15 +1000

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var timeSpan = DateTime.Now - TimeSpan.FromMinutes(1);
+                var di = new DirectoryInfo($"C:\\Users\\JzK\\Desktop\\Test");
+                if (di.Exists)
+                {
+                    Console.WriteLine("File Exists");
+                    foreach (DirectoryInfo directory in di.GetDirectories())
+                    {
+                        if (directory.LastAccessTime < timeSpan) {directory.Delete();}
+                    }
+                    foreach (FileInfo file in di.GetFiles())
+                    {
+                        if (file.LastAccessTime < timeSpan) { file.Delete(); }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("File not found");
+                }
+            }
+            catch (Exception ex) { Console.WriteLine("You haven't permission to change files"); }
+        }
+    }
+}
