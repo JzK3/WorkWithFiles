58ad69aea66b0c0d8b48c20e443ab5c43db299c2
JzK <andre-kiryushin@mail.ru>
2024-07-24 15:16:02 +1000

    Added task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Encodings.Web;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = @"C:\\Users\\JzK\\Desktop\\Test";
+                if (Directory.Exists(path))
+                {
+                    var size = new DirectoryInfo(path);
+                    DirSize(size);
+                }
+                else
+                {
+                    Console.WriteLine("File not found");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("You haven't permission to change files");
+            }
+            }
+            static long DirSize(DirectoryInfo dir)
+        {
+            long Size = 0;
+            FileInfo[] fis = dir.GetFiles();
+            foreach (FileInfo fi in fis)
+            {
+                Size += fi.Length;
+            }
+
+            DirectoryInfo[] dis = dir.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                Size += DirSize(di);
+            }
+            if (Size > 0) { Console.WriteLine($"Total size of files and directories {Size} bytes"); }
+            return Size;
+        }
+    }
+}
